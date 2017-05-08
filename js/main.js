b3061e3ac620254be4981cf69c38549ea89703d0
Cody Swift <ccswift@me.com>
2017-05-07 21:35:04 -0400

    finishing touches and basic form validation

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var submit = document.querySelector("#mySubmit")
+var form = document.forms[0]
+
+
+form.addEventListener('input', function(){
+  var name = form.name.value;
+  var email = form.email.value;
+  var comments = form.comments.value;
+  if(name != "" && email != "" && comments !="" && email.match("@")){
+    submit.classList.remove("invalid");
+  }
+  else if(submit.classList.value != "invalid"){
+    submit.classList.add("invalid");
+  }
+})
